0a0a895e2b1af56de7317c516f1b1a48f90a64fa
sontran283 <tranhongson283@gmail.com>
2023-12-05 22:35:57 +0700

    up

--- gradlew
+++ gradlew
@@ -22,7 +22,7 @@
 #
 #   Important for running:
 #
-#   (1) You need a POSIX-compliant shell to run this script. If your /bin/sh is
+#   (tpl) You need a POSIX-compliant shell to run this script. If your /bin/sh is
 #       noncompliant, but you have some other compliant shell such as ksh or
 #       bash, then to run this script, type that shell name before the whole
 #       command line, like:
@@ -88,7 +88,7 @@ APP_BASE_NAME=${0##*/}
 # Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 DEFAULT_JVM_OPTS='"-Xmx64m" "-Xms64m"'
 
-# Use the maximum available, or set MAX_FD != -1 to use that value.
+# Use the maximum available, or set MAX_FD != -tpl to use that value.
 MAX_FD=maximum
 
 warn () {
--- src/main/java/com/ra/controller/user/HomeController.java
+++ src/main/java/com/ra/controller/user/HomeController.java
@@ -7,7 +7,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
+    public String home() {
         return "user/home";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "user/about";
+    }
+
+    @RequestMapping("/block")
+    public String block() {
+        return "user/block";
+    }
+
+    @RequestMapping("/cart")
+    public String cart() {
+        return "user/cart";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "user/contact";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+    @RequestMapping("/page")
+    public String page() {
+        return "user/page";
+    }
+
+    @RequestMapping("/shop")
+    public String shop() {
+        return "user/shop";
+    }
+
+    @RequestMapping("/wishlist")
+    public String wishlist() {
+        return "user/wishlist";
     }
 }
