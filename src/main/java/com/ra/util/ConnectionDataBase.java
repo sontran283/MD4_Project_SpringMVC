1434c3f0298a7ad85516f9e6f11ac1c959ff18cb
sontran283 <tranhongson283@gmail.com>
2023-12-07 00:34:03 +0700

    ghgh

--- src/main/java/com/ra/util/ConnectionDataBase.java
+++ src/main/java/com/ra/util/ConnectionDataBase.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDataBase {
-    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String URL = "jdbc:mysql://localhost:3306/project_module4_webfruit";
     private static final String USER = "root";
     private static final String PASSWORD = "11111111";
 
